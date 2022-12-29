3ef1b591d8e8811794b6c692a17674877d3f779c
yadsyah <diyansetiyadi@gmail.com>
2022-12-29 17:47:02 +0700

    init commit

---
+++ src/test/java/id/co/diansetiyadi/billpaymentservice/BillPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.billpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
